cc8ed8756ccc6501b5c0470912eff4edf0bfa8fc
utsavui <utsavgupta1356@gmail.com>
2025-12-08 00:08:36 -0800

    projects

--- src/hello-intern.js
+++
@@ -1,7 +0,0 @@
-// src/hello-intern.js
-// This is a simple script to confirm your Node.js environment is set up correctly.
-
-console.log("-----------------------------------------");
-console.log("🚀 Hello staytuned ");
-console.log("-----------------------------------------");
-console.log("I am a App devloper");
